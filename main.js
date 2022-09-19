932bb048b7f0d37e04d458bcf1dc6dc998ae01c0
Diego Mejia <Diego.mejia@pascualbravo.edu.co>
2022-09-18 23:42:49 -0500

    configuracion de menu hamburguesa para mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamMobile_icon = document.querySelector(".menu");
+const menuHamMobile = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toogleDesktopMenu)
+menuHamMobile_icon.addEventListener('click', toogleMobileMenu)
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    menuHamMobile.classList.toggle('inactive');
 }
